44b8e1ca224eb6f988b8f32a7a9fe1d00f228f67
DavidHenrique2106 <dh210605@gmail.com>
2023-09-03 20:44:06 -0300

    Utilizando o laço de repetição "FOR"

---
+++ Atividades/src/laço.java
@@ -0,0 +1,14 @@
+package Atividades.src;
+
+public class laço{
+
+    public static void main(String[] args){
+
+        for(int c = 30; c>=1; c--)
+        System.out.print(c + " ");
+    }
+
+
+}
+    
+
