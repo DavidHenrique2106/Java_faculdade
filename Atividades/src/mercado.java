64b9d4913124a6c41f8bbd3f06f81f56fda9136c
DavidHenrique2106 <dh210605@gmail.com>
2023-09-22 22:51:38 -0300

    Atividade

---
+++ Atividades/src/mercado.java
@@ -0,0 +1,26 @@
+package Atividades.src;
+import java.util.Scanner;
+public class mercado{
+        public static void main(String[] args){
+
+            String produto;
+            int quantidade, valor_unitário;
+            Scanner ler = new Scanner(System.in);
+
+            System.out.println("Digite o produto que você pegou -> ");
+            produto = ler.next();
+
+            System.out.println("Quanto você produtos você pegou -> ");
+            quantidade = ler.nextInt();
+
+            System.out.println("Digite qual o valor unitário do produto -> ");
+            valor_unitário = ler.nextInt();
+
+            if (quantidade <=5){
+                System.out.println("Nome do produto -> "+ (produto));
+                System.out.println("Valor da compra -> "+ (quantidade * valor_unitário));
+                System.out.println("Desconto de 2% ");
+                System.out.println("Valor final -> " );
+            }
+    }
+}
