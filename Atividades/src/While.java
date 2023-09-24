67e488e42f9c5e92d8097a6d12772c509402c233
DavidHenrique2106 <dh210605@gmail.com>
2023-09-24 12:01:45 -0300

    Usando while

---
+++ Atividades/src/While.java
@@ -0,0 +1,17 @@
+package Atividades.src;
+
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args){
+
+
+            int contador = 0;
+
+            while (contador < 6){
+                System.out.println("Contagem -> "+ contador);
+
+                contador++;
+            }
+    }
+}
