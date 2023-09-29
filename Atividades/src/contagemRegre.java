2919ad039cff025010c0e07dee0c4bd99a89a276
DavidHenrique2106 <dh210605@gmail.com>
2023-09-29 10:47:16 -0300

    Contagem regressiva

---
+++ Atividades/src/contagemRegre.java
@@ -0,0 +1,16 @@
+package Atividades.src;
+
+public class contagemRegre {
+    public static void main(String[] arsg){
+
+        int contador = 10;
+
+
+        
+        while (contador >= 0) {
+            System.out.println(contador);
+            contador--;
+        }
+
+    }
+}
