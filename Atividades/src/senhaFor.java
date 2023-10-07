5e00b0e6fd221ea2a9bb2cc7877bb660255b4274
DavidHenrique2106 <dh210605@gmail.com>
2023-10-07 10:10:58 -0300

    Senha usando for

--- Atividades/src/senhaFor.java
+++ Atividades/src/senhaFor.java
@@ -1,3 +1,30 @@
-public class senhaFor {
-    
+import java.util.Scanner;
+
+public class senhaFor{
+    public static void main(String[] args ){
+
+        int tentativas = 5;
+        String senhaCerta = "12345", senha;
+
+        Scanner ler = new Scanner(System.in);
+
+        for (int i=1; i <=tentativas; i++){
+
+        System.out.println("Digite a sua senha -> ");
+        senha = ler.nextLine();
+
+        if (senha.equals(senhaCerta)){
+            System.out.println("Acesso permitido ");
+            break;
+        }
+
+        else{
+            System.out.println("Acesso negado. Tentativas restantes -> "+ (tentativas - i));
+            if (i == tentativas){
+                System.out.println("Acesso negado");
+            }
+        }
+
+        }
+    }
 }
