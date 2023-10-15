e4d51b5eab41e349470e43c8b7205e040c97d910
DavidHenrique2106 <dh210605@gmail.com>
2023-10-14 22:20:28 -0300

    usando condicionais para melhorar o exercício

--- Atividades/src/conversão.java
+++ Atividades/src/conversão.java
@@ -8,11 +8,22 @@ public class conversão {
             Scanner ler = new Scanner(System.in);
 
             double ValorDolar = 5.08;
+            double ValorReal;
 
             System.out.println("Digite quantos você tem em Reais (R$) -> ");
-            double ValorReal = ler.nextDouble();
+            ValorReal = ler.nextDouble();
 
-            System.out.println("Você tem U$$ Dólares na conversão -> "+ (ValorReal / ValorDolar));
+            double conversao = (ValorReal)/ValorDolar;
+
+            System.out.println("Você tem U$$ Dólares na conversão -> "+ (conversao));
+
+            if (conversao < 1500){
+                System.out.println("Viagem indisponível. Valor mínimo não atingido");
+            }
+
+            else{
+                System.out.println("Viagem aceita. Boas ferias!");
+            }
 
     }
 }
