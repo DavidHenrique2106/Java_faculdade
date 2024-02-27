62b1157e7cf98c349c9444841378664a283e5cfb
DavidHenrique2106 <dh210605@gmail.com>
2024-02-26 21:33:59 -0300

    feat: introdução a matriz com java

---
+++ Atividades/src/introMatriz.java
@@ -0,0 +1,15 @@
+public abstract class introMatriz {
+    
+    public static void main(String[] args){
+        
+            for (int i = 1; i <= 3; i++){
+                for (int j = 0; j <= 3; j++){
+                    System.out.print( (i-1) *3 + j + 1 );
+                }
+                System.out.println("\n");
+            }
+
+
+    } 
+
+}
