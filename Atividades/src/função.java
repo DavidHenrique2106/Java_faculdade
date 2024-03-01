a8fe5f40d602679651a6fefe0bec059faf8c2598
DavidHenrique2106 <dh210605@gmail.com>
2024-02-29 21:28:18 -0300

    funcao faculdade

---
+++ Atividades/src/função.java
@@ -0,0 +1,16 @@
+public class função {
+
+    public static int soma(int a, int b){
+        int resultado = a+b;
+        return resultado;
+    }
+
+    public static void main(String[] args){
+
+        int res = soma(2,4);
+        
+        System.out.println(res);
+
+
+    }
+}
